ad7869e8f5f9ec45c42fdeafb989aaf273c5b2a8
SohailaHamed1 <hsohaila56@gmail.com>
2023-11-24 05:05:02 +0200

    add all posts git

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def post_list(request):
+    data = Post.objects.all()
+    return render(request,'posts.html',{'posts':data})
