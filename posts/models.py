8beafdf68505e01b92e5f0e19d667edd0d63f694
SohailaHamed1 <hsohaila56@gmail.com>
2023-11-24 02:45:48 +0200

    add post tags

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=50000)
     puplish_date = models.DateTimeField()
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
